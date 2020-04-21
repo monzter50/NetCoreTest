e1b74ee9899158b891bfd1d01dde7e853e9f7d6e
jose gonzalez <josemonzter50@gmail.com>
2020-04-20 20:34:27 -0500

    first commit

--- AppCourseSaleContext.cs
+++ AppCourseSaleContext.cs
@@ -7,7 +7,7 @@ namespace NetCore.Data
     public class AppCourseSaleContext:DbContext
     {
         public DbSet<Course> Course { get; set; }
-        private const string connectionMySql = @"Server=localhost;database=CoursesOnline;uid=root;pwd=sinmiedo;";
+        private const string connectionMySql = @"Server=localhost;database=;uid=;pwd=;";
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder
